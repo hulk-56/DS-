11a2dae5e07d667399e75bd2181f93cf0f4d5c86
hulk-56 <60963534+hulk-56@users.noreply.github.com>
2020-09-15 17:48:43 +0530

    Create Practical1b.py

---
+++ Practical1b.py
@@ -0,0 +1,46 @@
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[1, 2], [3, 4]]
+mat3 = [[0, 0], [0, 0]]
+
+for i in range(0, 2):
+    for j in range(0, 2):
+        mat3[i][j] = mat1[i][j] + mat2[i][j]
+
+for i in range(0, 2):
+    for j in range(0, 2):
+        print(mat3[i][j], end="")
+print()
+
+#multiplication
+mat1 = [[10, 9], [8, 6]]
+mat2 = [[1, 2], [3, 4]]
+mat3 = [[0, 0], [0, 0]]
+
+for i in range(0, 2):
+    for j in range(0, 2):
+        mat3[i][j] = mat1[i][j] * mat2[i][j]
+
+
+for i in range(len(mat1)):
+    for j in range(len(mat2[0])):
+        for k in range(len(mat2)):
+            mat3[i][j] =mat3[i][j] + ( mat1[i][k] * mat2[k][j])
+
+
+for r in mat3:
+    print(r)
+    
+ #transpose
+ X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+for i in range(len(X)):
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+
+for r in result:
+   print(r)
